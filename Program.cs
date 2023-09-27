b8ee70b1d09dac14927d29905891380f7dfccf82
Your Name <katsia.mir@gmail.com>
2023-09-27 13:13:42 +0300

    Seminar2

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿void HelloFunc(string name){
+    Console.WriteLine($"Hello, {name}! How are you?");
+}
+Console.WriteLine("Hi, what is your name?");
+string fio = Console.ReadLine();
+HelloFunc(fio);
